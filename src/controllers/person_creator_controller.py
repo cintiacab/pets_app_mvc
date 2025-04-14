34091af96ae1cd13ecec2df682904d1234b269fe
Cintia Cabral <cintiacabral@hotmail.com>
2025-04-14 16:12:19 -0300

    Implementing controllers Interfaces

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,8 +1,9 @@
 from typing import Dict
 import re
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface # type: ignore
+from .interfaces.person_creator_controller import PersonCreatorControllerInterface # type: ignore
 
-class PersonCreatorController:
+class PersonCreatorController(PersonCreatorControllerInterface):
     def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
         self.__people_repository = people_repository
     
--- src/controllers/pet_lister_controller.py
+++ src/controllers/pet_lister_controller.py
@@ -1,8 +1,9 @@
 from typing import Dict, List
 from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
 from src.models.sqlite.entities.pets import PetsTable # type: ignore
+from .interfaces.pet_lister_controller import PetListerControllerInterface # type: ignore
 
-class PetListerController:
+class PetListerController(PetListerControllerInterface):
     def __init__(self, pet_repository:PetsRepositoryInterface) -> None:
         self.__pet_repository = pet_repository
 
