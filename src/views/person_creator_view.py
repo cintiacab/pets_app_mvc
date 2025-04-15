60d13581720d349835827ecc936dfb2d108bfe89
Cintia Cabral <cintiacabral@hotmail.com>
2025-04-15 18:05:08 -0300

    feat: Implementing validators dir using pydantic

--- src/views/person_creator_view.py
+++ src/views/person_creator_view.py
@@ -1,4 +1,5 @@
 from src.controllers.interfaces.person_creator_controller import PersonCreatorControllerInterface # type: ignore
+from src.validators.person_creator_validator import person_creator_validator
 from src.views.http_types.http_request import HttpRequest # type: ignore
 from src.views.http_types.http_response import HttpResponse # type: ignore
 from .interfaces.view_interface import ViewInterface # type: ignore
@@ -8,6 +9,8 @@ class PersonCreatorView(ViewInterface):
         self.__controller = controller
     
     def handle(self, http_request: HttpRequest) -> HttpResponse:
+        person_creator_validator(http_request)
+
         person_info = http_request.body
         body_response = self.__controller.create(person_info)
         
